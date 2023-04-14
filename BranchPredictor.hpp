a914c2e7c533e06c62cdf09f8eafd00f089b42b2
Mihir K <mihirk803@gmail.com>
2023-04-14 17:27:03 +0530

    Not much changes

--- BranchPredictor.hpp
+++ BranchPredictor.hpp
@@ -88,13 +88,63 @@ struct SaturatingBHRBranchPredictor : public BranchPredictor {
 
     bool predict(uint32_t pc) {
         // your code here
-        return false;
+        int val=(int)(bhr.to_ulong());
+        uint32_t lsb14=(pc & (uint32_t)((1<<14)-1));
+
+        if((bhrTable[val]==bitset<2>(3)) && (table[lsb14]==bitset<2>(3))) return true;
+        else if((bhrTable[val]==bitset<2>(3)) && (table[lsb14]==bitset<2>(2))) return true;
+        else if((bhrTable[val]==bitset<2>(2)) && (table[lsb14]==bitset<2>(3))) return true;
+        else if((bhrTable[val]==bitset<2>(2)) && (table[lsb14]==bitset<2>(2))) return true;
+
+        if(((bhrTable[val]==bitset<2>(0))) && (table[lsb14]==bitset<2>(0))) return false;
+        else if((bhrTable[val]==bitset<2>(0)) && (table[lsb14]==bitset<2>(1))) return false;
+        else if((bhrTable[val]==bitset<2>(1)) && (table[lsb14]==bitset<2>(0))) return false;
+        else if((bhrTable[val]==bitset<2>(0)) && (table[lsb14]==bitset<2>(0))) return false;
+
+        else
+        {
+            int rnd=rand()%2;
+            if(rnd) return true;
+            else return false;
+        }
     }
 
     void update(uint32_t pc, bool taken) {
         // your code here
 
-        
+        uint32_t lsb14=(pc & (uint32_t)((1<<14)-1));
+
+        if(taken)
+        {
+            if(table[lsb14]==bitset<2>(0)) table[lsb14]=bitset<2>(1);
+            else if(table[lsb14]==bitset<2>(1)) table[lsb14]=bitset<2>(2);
+            else if(table[lsb14]==bitset<2>(2)) table[lsb14]=bitset<2>(3);
+        }
+        else
+        {
+            if(table[lsb14]==bitset<2>(1)) table[lsb14]=bitset<2>(0);
+            else if(table[lsb14]==bitset<2>(2)) table[lsb14]=bitset<2>(1);
+            else if(table[lsb14]==bitset<2>(3)) table[lsb14]=bitset<2>(2);
+        }
+
+        int val=(int)(bhr.to_ulong());
+
+        if(taken)
+        {
+            if(bhrTable[val]==bitset<2>(0)) bhrTable[val]=bitset<2>(1);
+            else if(bhrTable[val]==bitset<2>(1)) bhrTable[val]=bitset<2>(2);
+            else if(bhrTable[val]==bitset<2>(2)) bhrTable[val]=bitset<2>(3);
+        }
+        else
+        {
+            if(bhrTable[val]==bitset<2>(1)) bhrTable[val]=bitset<2>(0);
+            else if(bhrTable[val]==bitset<2>(2)) bhrTable[val]=bitset<2>(1);
+            else if(bhrTable[val]==bitset<2>(3)) bhrTable[val]=bitset<2>(2);
+        }
+        val=val%2;
+        val=val*2;
+        if(taken) val+=1;
+        bhr=bitset<2>(val);
     }
 };
 
